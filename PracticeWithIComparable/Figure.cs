dc8193fda1c797702c7e9b6996a583cd1b86eae6
MVasili 34 <vasili.dubov@gmail.com>
2023-06-10 20:26:18 +0300

    IComparable - сравнение объектов

---
+++ PracticeWithIComparable/Figure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWithIComparable
+{
+	public class Figure : IComparable<Figure>
+	{
+		private int length;
+		private int square;
+		public Figure() { }
+		public Figure(int length)
+		{
+			Length = length;
+			square = length * length;
+		}
+
+		public int Square
+		{
+			get => square;
+		}
+
+		public int Length
+		{
+			get => length;
+			set => length = value;
+		}
+		public int CompareTo(Figure? other)
+		{
+			if (other is null)
+			{
+				throw new ArgumentNullException("Other in not Figure object");
+			}
+			else
+				return this.Length.CompareTo(other.Length);
+		}
+	}
+}
---
+++ PracticeWithIComparable/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace PracticeWithIComparable
+{
+	internal class Program
+	{
+		static IEnumerable<Figure> SomeSquares => Enumerable.Range(1, 10).Select(i => new Figure(Random.Shared.Next(5, 50)));
+		static void Main(string[] args)
+		{
+			List<Figure> list = SomeSquares.ToList();
+			WriteLine("Список фигур до применения сортировки");
+			foreach (Figure f in list)
+			{
+				WriteLine($"Квадрат с ребром: {f.Length} и площадью: {f.Square}");
+			}
+
+			list.Sort();
+			list.Reverse();
+
+			WriteLine("Список фигур после применения сортировки");
+			foreach (Figure f in list)
+			{
+				WriteLine($"Квадрат с ребром: {f.Length} и площадью: {f.Square}");
+			}
+			ReadKey();
+		}
+	}
+}
