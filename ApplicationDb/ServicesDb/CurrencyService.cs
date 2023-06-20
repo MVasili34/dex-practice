309e3fb1dc85d5af769484329523694492d6b406
MVasili 34 <vasili.dubov@gmail.com>
2023-06-20 18:49:49 +0300

    Класс HttpClient и отправка запросов

---
+++ ApplicationDb/ServicesDb/CurrencyService.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesDb;
+
+public class CurrencyService
+{
+	private readonly string apikey = null!;
+	public CurrencyService(string apikey)
+	{
+		this.apikey = apikey;
+	}
+
+	public async Task<AmdorenResponse> Convert(ConvertCurrency convertCurrency)
+	{
+		HttpClient httpClient = new HttpClient();
+		Uri request;
+		if (convertCurrency.Amount==0)
+		{
+			request = new($"https://www.amdoren.com/api/currency.php?api_key={apikey}&" +
+				$"from={convertCurrency.From}&to={convertCurrency.To}");
+		}
+		else
+		{
+			request = new($"https://www.amdoren.com/api/currency.php?api_key={apikey}" +
+				$"&from={convertCurrency.From}&to={convertCurrency.To}&amount={convertCurrency.Amount}");
+		}
+		HttpResponseMessage responseMessage = await httpClient.GetAsync(request);
+		responseMessage.EnsureSuccessStatusCode();
+		string message = await responseMessage.Content.ReadAsStringAsync();
+		return JsonConvert.DeserializeObject<AmdorenResponse>(message)!;
+	}
+}
+
+public class ConvertCurrency
+{
+	public string From { get; set; } = null!;
+	public string To { get; set; } = null!;
+	public decimal Amount { get; set; }
+}
+
+public class AmdorenResponse
+{
+	public int Error { get; set; }
+	public string Error_Message { get; set; } = null!;
+	public decimal Amount { get; set; }
+}
---
+++ ApplicationDb/ServicesDbTests.Tests/ConvertCurrencyTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using ServicesDb;
+
+namespace ServicesDbTests.Tests;
+
+public class ConvertCurrencyTest
+{
+	private readonly ITestOutputHelper Output;
+
+	public ConvertCurrencyTest(ITestOutputHelper Output)
+	{
+		this.Output = Output;
+	}
+
+	[Fact] //тестирование сервиса, не передавая сумму (выводится курс для 1 доллара)
+	public async Task GetExschangeRateTest()
+	{
+		ConvertCurrency convert = new() { From = "EUR", To = "USD" };
+		CurrencyService service = new("L4tz65xgEvSuCcSPnYjzjBzhU4EAWd");
+		AmdorenResponse response = await service.Convert(convert);
+		Output.WriteLine($"Error: {response.Error}-{response.Error_Message}, Amount: {response.Amount}");
+	}
+
+	[Fact] //тестирование сервиса, передавая сумму
+	public async Task ConvertAmountTest()
+	{
+		ConvertCurrency convert = new() { From = "USD", To = "RUB", Amount = 10 };
+		CurrencyService service = new("L4tz65xgEvSuCcSPnYjzjBzhU4EAWd");
+		AmdorenResponse response = await service.Convert(convert);
+		Output.WriteLine($"Error: {response.Error}-{response.Error_Message}, Amount: {response.Amount}");
+	}
+
+	[Fact] //тестирование серевиса, передавая неверный API ключ
+	public async Task ApiKeyFailureTest()
+	{
+		ConvertCurrency convert = new() { From = "USD", To = "RUB", Amount = 10 };
+		CurrencyService service = new("12345");
+		AmdorenResponse response = await service.Convert(convert);
+		Assert.Equal(110, response.Error);
+	}
+
+	[Fact] //тестирование курса, передавая неверное значение суммы
+	public async Task WrongAmountTest()
+	{
+		ConvertCurrency convert = new() { From = "USD", To = "RUB", Amount = (decimal)-10.5 };
+		CurrencyService service = new("L4tz65xgEvSuCcSPnYjzjBzhU4EAWd");
+		AmdorenResponse response = await service.Convert(convert);
+		Assert.Equal(300, response.Error);
+	}
+}
