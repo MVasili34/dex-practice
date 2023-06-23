0be4e41e9711ade1c39d1b439ff7e490406c37c1
MVasili 34 <vasili.dubov@gmail.com>
2023-06-24 01:23:46 +0300

    Добавлено Blazor-приложение

--- API/BankAPI/Program.cs
+++ API/BankAPI/Program.cs
@@ -11,8 +11,9 @@ namespace BankAPI
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			//Adding DataBase context
-			builder.Services.AddBankingServiceContext();
+			builder.WebHost.UseUrls("https://localhost:5001/");
+                                          //Adding DataBase context
+            builder.Services.AddBankingServiceContext();
 
 			// Add services to the container.
 			builder.Services.AddControllers().AddJsonOptions(x =>
@@ -23,9 +24,15 @@ namespace BankAPI
 			builder.Services.AddSwaggerGen();
 			builder.Services.AddScoped<IClientService, ClientService>();
 			builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-			
-			var app = builder.Build();
 
+            builder.Services.AddCors();
+			var app = builder.Build();
+			app.UseCors(options =>
+			{
+				options.WithMethods("GET", "POST", "PUT", "DELETE");
+				options.WithOrigins("https://localhost:5005/");
+
+            });
 			// Configure the HTTP request pipeline.
 			app.UseHttpLogging();
 			if (app.Environment.IsDevelopment())
---
+++ BlazorDemoBanking/BankingService/Program.cs
@@ -0,0 +1,49 @@
+using BankingService.Data;
+using EntityModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http.Headers;
+using Radzen;
+
+namespace BankingService
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddBankingServiceContext();
+			// Add services to the container.
+			builder.Services.AddRazorPages();
+			builder.Services.AddServerSideBlazor();
+			builder.Services.AddSingleton<WeatherForecastService>();
+			builder.Services.AddHttpClient("BankAPI", options =>
+			{
+				options.BaseAddress = new Uri("https://localhost:5001/");
+				options.DefaultRequestHeaders.Accept.Add(
+					new MediaTypeWithQualityHeaderValue("application/json", 1.0));
+			});
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.MapBlazorHub();
+			app.MapFallbackToPage("/_Host");
+
+			app.Run();
+		}
+	}
+}
