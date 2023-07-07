ab0691cc5dbd1e48b1656c69454b5fd038ecc357
MVasili 34 <vasili.dubov@gmail.com>
2023-07-07 19:02:21 +0300

    Настроено HTTP логирование

--- API/BankAPI/Program.cs
+++ API/BankAPI/Program.cs
@@ -3,52 +3,55 @@ using ServicesDb;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.HttpLogging;
 
-namespace BankAPI
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("https://localhost:5001/");
+
+//Добавляем контекст БД
+builder.Services.AddBankingServiceContext();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+	x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+builder.Services.AddCors();
+
+//подключаем HTTP логирование
+builder.Services.AddHttpLogging(options =>
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+	options.RequestHeaders.Add("Origin");
 
-			builder.WebHost.UseUrls("https://localhost:5001/");
-                                          //Adding DataBase context
-            builder.Services.AddBankingServiceContext();
+	options.LoggingFields = HttpLoggingFields.All;
+});
 
-			// Add services to the container.
-			builder.Services.AddControllers().AddJsonOptions(x =>
-				x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+var app = builder.Build();
 
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-			builder.Services.AddScoped<IClientService, ClientService>();
-			builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+// Configure the HTTP request pipeline.
+app.UseCors(options =>
+{
+	options.WithMethods("GET", "POST", "PUT", "DELETE");
+	options.WithOrigins("https://localhost:5005/");
 
-            builder.Services.AddCors();
-			var app = builder.Build();
-			app.UseCors(options =>
-			{
-				options.WithMethods("GET", "POST", "PUT", "DELETE");
-				options.WithOrigins("https://localhost:5005/");
+});
 
-            });
-			// Configure the HTTP request pipeline.
-			app.UseHttpLogging();
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-
-			app.MapControllers();
-
-			app.Run();
-		}
-	}
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseHttpLogging();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
